f9dd06e3617fcbaf7393d1913cde9760717ae7ee
SAHIL VARSHNEY <145244634+25sahilvarshney@users.noreply.github.com>
2026-06-09 00:16:35 +0530

    sahil varshney

---
+++ core-java-practice/gcr-codebase/programming-elements/VolumeOfACylinder.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class VolumeCylinder {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter radius: ");
+        double radius = sc.nextDouble();
+
+        System.out.print("Enter height: ");
+        double height = sc.nextDouble();
+
+        double volume = 3.14 * radius * radius * height;
+
+        System.out.println("Volume of Cylinder is: " + volume);
+    }
+}
