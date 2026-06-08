34c152ca2fd00f72bd312559f709617029c7b651
sahil varshney <sahilvvs23@gmail.com>
2026-06-09 00:12:34 +0530

    sahil varshney

---
+++ core-java-practice/gcr-codebase/programming-elements/RaviGuildMember.java
@@ -0,0 +1,11 @@
+public class RaviGuildMember{
+    public static void main(String[] args){
+        String name = "Ravi";
+        int age = 28;
+        char rank = 'A';
+        float membershipFee = 700.28f;
+        int salary = 100000;
+        int annual_Bonus = (int)(salary*0.12);
+        System.out.println("the annual bonus for ravi is:"+annual_Bonus);
+    }
+}
