83143970b88a6b909963b9b03eefef80cb9e15f1
sahil varshney <sahilvvs23@gmail.com>
2026-06-16 13:19:11 +0530

    [factorial]

---
+++ core-java-practice/gcr-codebase/Recursion/Level-1/factorial.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public  class factorial{
+    public static int factorial(int n){
+        if(n == 0 || n == 1){
+            return 1;
+        }
+        return n * factorial(n - 1);
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number:");
+        int n = sc.nextInt();
+        int result = factorial(n);
+        System.out.println("Factorial of " + n + " is: " + result);
+    }
+}
