10a9c031d31ca4e8b11b3f7f6ac75b9de1e15202
sahil varshney <sahilvvs23@gmail.com>
2026-06-16 13:41:34 +0530

    [ReverseString]

---
+++ core-java-practice/gcr-codebase/Recursion/Level-1/ReverseString.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class ReverseString {
+    public static String reverse(String str){
+        if(str.isEmpty()){
+            return str;
+        }
+        return reverse(str.substring(1)) + str.charAt(0);
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the string:");
+        String str = sc.nextLine();
+        String result = reverse(str);
+        System.out.println("Reversed string is: " + result);
+    }
+}
