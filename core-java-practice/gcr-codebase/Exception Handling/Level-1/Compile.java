9032f7c2177400ca5227465559c9b31b1f82f188
sahil varshney <sahilvvs23@gmail.com>
2026-06-17 13:16:33 +0530

    [checked exception]

--- core-java-practice/gcr-codebase/Exception Handling/Level-1/Compile.java
+++ core-java-practice/gcr-codebase/Exception Handling/Level-1/Compile.java
@@ -1,5 +1,18 @@
-
-
-public class Compile{
-    
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+public class complie{
+    public static void main(String[] args){
+        try{
+            BufferedReader read = new BufferedReader(new FileReader("data.txt"));
+            String line;
+            while((line = read.readLine())!=null){
+                System.out.println(line);
+            }
+            read.close();
+        }
+        catch (IOException e){
+            System.out.println("An IOException Occured:" + e.getMessage());
+        }
+    }
 }
