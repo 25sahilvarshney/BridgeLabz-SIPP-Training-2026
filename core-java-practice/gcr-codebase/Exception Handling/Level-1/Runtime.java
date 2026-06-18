e5a85723b46e4f0f71ef8b053e6053ffb5d0d573
SAHIL VARSHNEY <145244634+25sahilvarshney@users.noreply.github.com>
2026-06-19 00:41:47 +0530

    implement that code here

--- core-java-practice/gcr-codebase/Exception Handling/Level-1/Runtime.java
+++ core-java-practice/gcr-codebase/Exception Handling/Level-1/Runtime.java
@@ -1,5 +1,25 @@
 
+import java.util.Scanner;
+import java.util.InputMismatchException;
+public class User{
+    public static void main(String[] args){
+        try{
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Enter the first number:");
+            int n = sc.nextInt();
+            System.out.print("Enter the second number:");
+            int m = sc.nextInt();
+            int result = n / m;
+            System.out.print("division result:" + result);
 
-public class Runtime{
-    
+        }
+        catch (ArithmeticExceptio e){
+            System.out.println("An Arithmetic Exception Occured:");
+            
+        }
+        catch (InputMismatchException e){
+            System.out.println("An InputMismatchException Occured:");
+        }
+    }
+
 }
