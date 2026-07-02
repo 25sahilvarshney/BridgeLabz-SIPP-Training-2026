e8e8f277f12decb495441b92d74ea61bfc62d508
SAHIL VARSHNEY <145244634+25sahilvarshney@users.noreply.github.com>
2026-07-02 13:52:44 +0530

    tables names are updates

--- DBMS/Delete_Records.sql
+++ DBMS/Delete_Records.sql
@@ -1,16 +1,16 @@
-INSERT INTO covid_cases
+INSERT INTO covid_19_india_db
 (date, time, state, confirmed_indian_national,
 confirmed_foreign_national, recoveries, deaths, confirmed_cases)
 VALUES
 ('2020-03-18', '10:00:00', 'IN', 100, 10, 60, 2, 110);
 
-DELETE FROM covid_cases
+DELETE FROM covid_19_india_db
 WHERE state = 'IN';
 
-DELETE FROM covid_cases
+DELETE FROM covid_19_india_db
 WHERE id NOT IN
 (
     SELECT MIN(id)
-    FROM covid_cases
+    FROM covid_19_india_db
     GROUP BY state, date
 );
