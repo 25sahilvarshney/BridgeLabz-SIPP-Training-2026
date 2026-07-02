5408033ebae856ee909576888c438d66a011ed4c
SAHIL VARSHNEY <145244634+25sahilvarshney@users.noreply.github.com>
2026-07-02 13:41:13 +0530

    Update vaccination rates in covid_19_india table

--- DBMS/update_Vaccination.sql
+++ DBMS/update_Vaccination.sql
@@ -1,14 +1,14 @@
 ALTER TABLE covid_cases
 ADD vaccination_rate FLOAT;
 
-UPDATE covid_cases
+UPDATE covid_19_india
 SET vaccination_rate = 76.5
 WHERE state = 'India';
 
-UPDATE covid_cases
+UPDATE covid_19_india
 SET vaccination_rate = 82.1
 WHERE state = 'USA';
 
-UPDATE covid_cases
+UPDATE covid_19_india
 SET vaccination_rate = 79.4
 WHERE state = 'UK';
