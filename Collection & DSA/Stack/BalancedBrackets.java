aebc3c0eb61eeb001afacbf85ea8ea8adaf66b4e
sahil varshney <sahilvvs23@gmail.com>
2026-07-18 12:52:07 +0530

    Stack

---
+++ Collection & DSA/Stack/BalancedBrackets.java
@@ -0,0 +1,18 @@
+import java.util.Deque;
+import java.util.ArrayDeque;
+import java.util.Map;
+
+class ConfigValidator {
+    boolean isValidConfig(String s) {
+        Deque<Character> stack = new ArrayDeque<>();
+        Map<Character, Character> matchFor = Map.of(')', '(', ']', '[', '}', '{');
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '[' || c == '{') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty() || stack.pop() != matchFor.get(c)) return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+}
